700594ed82e39e2877fbaa433800356725fcd4ca
sakshi_er <sakshi.anand@lmsathena.com>
2026-02-25 10:04:13 +0530

    vercel file

--- react-tailwind-app/vite.config.js
+++ react-tailwind-app/vite.config.js
@@ -6,13 +6,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks: {
